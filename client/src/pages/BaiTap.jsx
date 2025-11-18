cf68e5cfe75dd163f4c44b9dec3c160a1ded611c
Yuuu Bin <daonguyennhatanh0910@gmail.com>
2025-11-18 15:12:16 +0700

    Update new

--- client/src/pages/BaiTap.jsx
+++ client/src/pages/BaiTap.jsx
@@ -29,15 +29,15 @@ function BaiTap() {
           </li>
           <li className="mb-5">
             <a href="https://tuan2-buoi2.onrender.com/" target="_blank">
-              Bài Tập Buổi 2
+              Bài Tập Buổi 2 - servlet
             </a>
           </li>
         </ul>
         <h2 className="text-3xl font-bold mb-5 ml-2">Tuần 3</h2>
         <ul className="p-10 border border-x-blue-600 border-y-violet-900 rounded-2xl">
           <li className="mb-5">
-            <a href="#" target="_blank">
-              .....
+            <a href="https://tuan3-buoi1.onrender.com" target="_blank">
+              Bài tập buổi 1 - Chương 6
             </a>
           </li>
           <li className="mb-5">
